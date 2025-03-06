5ab724b0c847d350befac6e4ff5fd8bfc9709863
123nadiiaserr <Grecha686@protonmail.com>
2025-03-06 14:51:51 +0100

    Create visualize.py

---
+++ visualize.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate random data
+np.random.seed(42)
+data = pd.DataFrame({
+    "X": np.arange(1, 11),
+    "Y": np.random.randint(10, 100, 10)
+})
+
+# Create plots
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+sns.lineplot(x="X", y="Y", data=data, marker="o")
+plt.title("Line Plot")
+
+plt.subplot(1, 2, 2)
+sns.barplot(x="X", y="Y", data=data)
+plt.title("Bar Chart")
+
+plt.show()
